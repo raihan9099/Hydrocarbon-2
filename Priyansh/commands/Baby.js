15bbabbdf89c32a29118dc45dc99e25bcee6a107
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-08-05 19:13:18 +0600

    Update Baby.js

--- Priyansh/commands/Baby.js
+++ Priyansh/commands/Baby.js
@@ -14,8 +14,8 @@ module.exports.config = {
  description: "better than all sim simi",
  commandCategory: "chat",
  category: "chat",
- usePrefix: true,
- prefix: true,
+ usePrefix: false,
+ prefix: false,
  usages: `[anyMessage] OR\nteach [YourMessage] - [Reply1], [Reply2], [Reply3]... OR\nteach [react] [YourMessage] - [react1], [react2], [react3]... OR\nremove [YourMessage] OR\nrm [YourMessage] - [indexNumber] OR\nmsg [YourMessage] OR\nlist OR\nall OR\nedit [YourMessage] - [NewMessage]`,
 };
 
@@ -156,7 +156,7 @@ try{
 module.exports.handleEvent = async function ({ api, event }) {
 try{
  const body = event.body ? event.body.toLowerCase() : ""
- if(body.startsWith("baby") || body.startsWith("bby") || body.startsWith("/bot")){
+ if(body.startsWith("😺") || body.startsWith("bby") || body.startsWith("/bot")){
  const arr = body.replace(/^\S+\s*/, "")
  if(!arr) {
  await api.sendMessage("hum xan bolo ami asi ", event.threadID, (error, info) => {
