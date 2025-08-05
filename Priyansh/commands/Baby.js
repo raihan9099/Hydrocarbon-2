9919bdf8ae631b179117f871313764df5e3461a0
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-08-05 21:18:12 +0600

    Update Baby.js

--- Priyansh/commands/Baby.js
+++ Priyansh/commands/Baby.js
@@ -14,9 +14,8 @@ module.exports.config = {
  description: "better than all sim simi",
  commandCategory: "chat",
  category: "chat",
- categoryprefix: 'noprefix'
- usePrefix: false,
- prefix: false,
+usePrefix: true,
+ prefix: true,
  usages: `[anyMessage] OR\nteach [YourMessage] - [Reply1], [Reply2], [Reply3]... OR\nteach [react] [YourMessage] - [react1], [react2], [react3]... OR\nremove [YourMessage] OR\nrm [YourMessage] - [indexNumber] OR\nmsg [YourMessage] OR\nlist OR\nall OR\nedit [YourMessage] - [NewMessage]`,
 };
 
